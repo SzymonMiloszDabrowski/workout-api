91efa49293e9df5f738bd0b9f160f3f5a33448d9
Szymon Dąbrowski <szymonmiloszdabrowski@gmail.com>
2021-02-13 10:11:39 +0100

    GET/POST users

---
+++ index.js
@@ -0,0 +1,30 @@
+const expreess = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+
+const app = expreess();
+app.use(cors());
+app.use(bodyParser.json());
+
+const usersRoute = require('./routes/users');
+
+app.use('/users', usersRoute);
+
+app.get('/', (req, res) => {
+  res.send('Workout API');
+});
+
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('Connected to DB!')
+);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+  console.log(`Link: http://localhost:${PORT}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+module.exports = router;
